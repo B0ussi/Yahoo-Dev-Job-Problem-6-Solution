6b5e36810be32c811fb2d2bd3f02e927b2815ce2
Noah Boussios <noahboussi@gmail.com>
2025-04-22 12:27:26 -0400

    answer from me --no clue if this is what they wanted--

---
+++ Javascript.js
@@ -0,0 +1,19 @@
+const string = "paypallishiring".toUpperCase();
+
+let y1 = [], y2 = [], y3 = [];
+
+for (const [i, letter] of Array.from(string).entries()) {
+  if (i === 0 || i % 4 === 0) {
+    y3.push(letter);
+  } else if (i === 1 || i % 2 === 1) {
+    y2.push(letter);
+  } else {
+    y1.push(letter);
+  }
+}
+
+console.log(`
+${y1.join(" ")}
+${y2.join("")}
+${y3.join(" ")}
+`);
